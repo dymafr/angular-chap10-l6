f34b04a1e396b663df2d443b4afadd86dc49de53
dymafr <erwan1309@hotmail.com>
2020-12-21 21:47:27 +0100

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { UsersComponent } from "./users/users.component";
+import { UserComponent } from './user/user.component';
 
 const APP_ROUTES: Routes = [
   { path: "", component: HomepageComponent },
-  { path: "users", component: UsersComponent }
+  { path: "users", component: UsersComponent },
+  { path: "users/:id", component: UserComponent }
 ];
 
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
-  declarations: [AppComponent, HomepageComponent, UsersComponent],
+  declarations: [AppComponent, HomepageComponent, UsersComponent, UserComponent],
   bootstrap: [AppComponent],
   providers: []
 })
