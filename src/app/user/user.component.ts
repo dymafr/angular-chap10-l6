4009ef7620ea5531ee64339101fd9de33dd51996
dymafr <erwan1309@hotmail.com>
2020-12-21 21:53:34 +0100

    2 files modified

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"]
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  public id: string;
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      this.id = paramMap.get("id");
+    });
   }
-
 }
